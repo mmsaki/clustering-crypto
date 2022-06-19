09d774c862ac92b921d2c7b30970bd488d5f63dc
mmsaki <meek10x@gmail.com>
2022-06-18 22:37:14 -0500

    adding lambda func

--- RoboAdvisor/lambda_function.py
+++ RoboAdvisor/lambda_function.py
@@ -26,45 +26,39 @@ def build_validation_result(is_valid, violated_slot, message_content):
         "message": {"contentType": "PlainText", "content": message_content},
     }
 
+### Start validation
 
- ### YOUR DATA VALIDATION CODE STARTS HERE ###
 def validate_data(age, investment_amount, intent_request):
     """
     Validates the data provided by the user.
     """
 
-   
-    # Validate that the user age is between 0 and 65 years old    
+    # Validate that the user is over 21 years old
     if age is not None:
         age = parse_int(age)
-        if age not in range (0,65):      
+        if age not in range (0,65):
             return build_validation_result(
                 False,
                 "age",
-                "Your age should be between 0 and 65 years old to use this service, "
+                "You should be at least between 1 and 65 years to use this service, "
                 "please provide a different age.",
             )
-        
-    # Validate the investment amount, it should be > 5,000
-    
+
+    # Validate the investment amount, it should be > 0
     if investment_amount is not None:
-        investment_amount = parse_int(
-            investment_amount
-        )  # Since parameters are strings it's important to cast values
-        if investment_amount <= 5000:
+        investment_amount = parse_int(investment_amount)  
+        if investment_amount < 5000:
             return build_validation_result(
                 False,
                 "investmentAmount",
-                "The investment amount should be greater than 5,000, "
-                "please provide a correct amount in dollars to invnest.",
+                "The amount to convert should be greater than 5000, "
+                "please provide a correct investment amount.",
             )
-    
-    
 
-    # A True results is returned if age or investment amount are valid
+    # A True results is returned if age or amount are valid
     return build_validation_result(True, None, None)
 
- ### YOUR DATA VALIDATION CODE ENDS HERE ###
+#### End validation
 
 
 ### Dialog Actions Helper Functions ###
@@ -90,7 +84,6 @@ def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message)
             "message": message,
         },
     }
-
 
 def delegate(session_attributes, slots):
     """
@@ -119,93 +112,86 @@ def close(session_attributes, fulfillment_state, message):
 
     return response
 
+
 ### Intents Handlers ###
 def recommend_portfolio(intent_request):
     """
     Performs dialog management and fulfillment for recommending a portfolio.
     """
-    # Gets slots' values
+
     first_name = get_slots(intent_request)["firstName"]
     age = get_slots(intent_request)["age"]
     investment_amount = get_slots(intent_request)["investmentAmount"]
     risk_level = get_slots(intent_request)["riskLevel"]
     source = intent_request["invocationSource"]
-    
 
-   
     if source == "DialogCodeHook":
         # Perform basic validation on the supplied input slots.
         # Use the elicitSlot dialog action to re-prompt
         # for the first violation detected.
-
-       
-      
-        # Gets all the slots
+        
+        ### YOUR FINAL INVESTMENT RECOMMENDATION CODE STARTS HERE ###
+        
+        # Get slots
         slots = get_slots(intent_request)
-        
-        # Validates user's input using the validate_data function
-        validation_result = validate_data(age, investment_amount, intent_request)
-        
-        
-        # If the data provided by the user is not valid,
-        # the elicitSlot dialog action is used to re-prompt for the first violation detected.
-        if not validation_result["isValid"]:
-            slots[validation_result["violatedSlot"]] = None  # Cleans invalid slot
 
-            # Returns an elicitSlot dialog to request new data for the invalid slot
+        # Validates user's input using the vaidate_data
+        validation_result = validate_data(age, investment_amount, intent_request)
+
+        # If the data provided by the user is not valid,
+        # the elicitSlot dialog
+        # action is used to re-prompt for the first violation detected.
+        if not validation_result["isValid"]:
+            slots[validation_result["violatedSlot"]] = None 
+            
+            # Return elicitSlot dialog to request new data for invalid slot
             return elicit_slot(
                 intent_request["sessionAttributes"],
                 intent_request["currentIntent"]["name"],
                 slots,
                 validation_result["violatedSlot"],
-                validation_result["message"],
+                validation_result["message"]
             )
-        
-
         # Fetch current session attibutes
         output_session_attributes = intent_request["sessionAttributes"]
-        
-        # Once all slots are valid, a delegate dialog is returned to Lex to choose the next course of action.
+
+        # Once all slots are valid, a delegate dialog is returned 
+        # to Lex to choose the next course of action
         return delegate(output_session_attributes, get_slots(intent_request))
-
+    
     # Get the initial investment recommendation
-
-    ### YOUR FINAL INVESTMENT RECOMMENDATION CODE STARTS HERE ###
-    # Gets all the slots
     slots = get_slots(intent_request)
-    
-    # Validates user's input using the validate_data function
+
+     # Validates user's input using the validate_data function
     validation_result = validate_data(age, investment_amount, intent_request)
-    
-    
     if not validation_result["isValid"]:
-            slots[validation_result["violatedSlot"]] = None  # Cleans invalid slot
+        slots[validation_result["violatedSlot"]] = None # Cleans invalid slot
+        # Returns an elicitSlot dialog to request 
+        # new data for the invalid slot
+        return elicit_slot(
+            intent_request["sessionAttributes"],
+            intent_request["currentIntent"]["name"],
+            slots,
+            validation_result["violatedSlot"],
+            validation_result["message"],
+        )
 
-            # Returns an elicitSlot dialog to request new data for the invalid slot
-            return elicit_slot(
-                intent_request["sessionAttributes"],
-                intent_request["currentIntent"]["name"],
-                slots,
-                validation_result["violatedSlot"],
-                validation_result["message"],
-            )
-         
-
-    
+    # Recommendation based on risk levels
     if risk_level == "None":
-        initial_recommendation = "100% bonds (AGG), 0% equities (SPY)"
+        initial_recommendation = "100% bonds (AGG), 0% equities (SPY)",
     elif risk_level == "Very Low":
-        initial_recommendation = "80% bonds (AGG), 20% equities (SPY)"
-    elif risk_level == "Low":
-        initial_recommendation = "60% bonds (AGG), 40% equities (SPY)"
+        initial_recommendation = "80% bonds (AGG), 20% equities (SPY)",
+    elif risk_level == "Low": 
+        initial_recommendation = "60% bonds (AGG), 40% equities (SPY)",
     elif risk_level == "Medium":
-        initial_recommendation = "40% bonds (AGG), 60% equities (SPY)"
+        initial_recommendation = "40% bonds (AGG), 60% equities (SPY)",
     elif risk_level == "High":
-        initial_recommendation = "20% bonds (AGG), 80% equities (SPY)"
+        initial_recommendation = "20% bonds (AGG), 80% equities (SPY)",
     else: initial_recommendation = "0% bonds (AGG), 100% equities (SPY)"
 
-
+   
     ### YOUR FINAL INVESTMENT RECOMMENDATION CODE ENDS HERE ###
+###################
 
     # Return a message with the initial recommendation based on the risk level.
     return close(
@@ -220,6 +206,7 @@ def recommend_portfolio(intent_request):
             ),
         },
     )
+
 
 ### Intents Dispatcher ###
 def dispatch(intent_request):
@@ -242,6 +229,5 @@ def lambda_handler(event, context):
     Route the incoming request based on intent.
     The JSON body of the request is provided in the event slot.
     """
-    print(f"Printing event")
-    print(event)
+
     return dispatch(event)
